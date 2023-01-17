aec3f5386f2d1e7ab9d715c39af6e8ac1d6decac
dinesh2003 <adhikaridinesh76@gmail.com>
2023-01-16 21:21:36 -0500

    finished PersonReader

--- src/PersonGenerator.java
+++ src/PersonGenerator.java
@@ -1,6 +1,65 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
 public class PersonGenerator {
     public static void main(String[] args)
     {
+        ArrayList<String> personCSVData = new ArrayList<>();
 
+        Scanner in = new Scanner(System.in);
+        String ID = "";
+        String firstName = "";
+        String lastName = "";
+        String title = "";
+        String CSVPersonRecord = "";
+        int YOB = 0;
+
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\PersonTestData.txt");
+
+        boolean done = false;
+        do {
+            ID = SafeInput.getNonZeroLenString(in, "Enter the ID as 6 digits");
+            firstName = SafeInput.getNonZeroLenString(in, "Enter the First Name");
+            lastName = SafeInput.getNonZeroLenString(in, "Enter the Last Name");
+            title = SafeInput.getNonZeroLenString(in, "Enter the Last Title");
+            YOB = SafeInput.getRangedInt(in,"Enter the year of birth as 4 digits", 1000, 9999);
+
+            CSVPersonRecord = ID + ", " + firstName + ", " + lastName + ", " + title + ", " + YOB;
+            personCSVData.add(CSVPersonRecord);
+
+            done = SafeInput.getYNConfirm(in, "Are you done entering Person data?");
+
+
+        } while (!done);
+
+        for (String p:personCSVData) {
+            System.out.println(p);
+        }
+        try
+        {
+            OutputStream out =
+                    new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            BufferedWriter writer =
+                    new BufferedWriter(new OutputStreamWriter(out));
+
+            for(String rec : personCSVData)
+            {
+                writer.write(rec, 0, rec.length());
+                writer.newLine();
+            }
+            writer.close();
+            System.out.println("Data file written!");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/PersonReader.java
@@ -0,0 +1,96 @@
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class PersonReader {
+    public static void main(String[] args) {
+        JFileChooser chooser = new JFileChooser();
+        File selectedFile;
+        String rec = "";
+        ArrayList<String> lines = new ArrayList<>();
+
+        final int FIELDS_LENGTH = 5;
+
+        String id, firstName, lastName, title;
+        int yob = 0;
+
+        try
+        {
+
+            File workingDirectory = new File(System.getProperty("user.dir"));
+
+            chooser.setCurrentDirectory(workingDirectory);
+
+            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+            {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+
+                InputStream in = new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+
+                int line = 0;
+                while(reader.ready())
+                {
+                    rec = reader.readLine();
+                    lines.add(rec);
+                    line++;
+                    System.out.println(rec);
+
+                }
+                reader.close();
+                System.out.println();
+
+
+                System.out.print(String.format("%-10s%-15s%-15s%-6s%6s", "ID#", "FirstName", "LastName", "Title", "YOB"));
+                System.out.print("\n========================================================");
+                String[] fields;
+                for(String l:lines)
+                {
+                    fields = l.split(",");
+                    if(fields.length == FIELDS_LENGTH)
+                    {
+
+                        id = fields[0].trim();
+                        firstName = fields[1].trim();
+                        lastName = fields[2].trim();
+                        title = fields[3].trim();
+                        yob = Integer.parseInt(fields[4].trim());
+                        System.out.print(String.format("\n%-10s%-15s%-15s%-6s%6d", id, firstName, lastName, title, yob));
+                    }
+                    else
+                    {
+                        System.out.println("Found a record that may be corrupt: ");
+                        System.out.println();
+                    }
+                }
+                System.out.println();
+            }
+            else
+            {
+                System.out.println("Failed to choose a file to process");
+                System.out.println("Run the program again!");
+                System.exit(0);
+            }
+        }  // end of TRY
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File not found!!!");
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+
+
